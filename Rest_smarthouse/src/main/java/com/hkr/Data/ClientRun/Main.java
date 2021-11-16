7fc0e8caa9dced05179906b48deab84c9c6e49f7
Gormsson <gormsson2@gmail.com>
2021-11-16 08:30:21 +0100

    Update Main.java
    changed line in ClientRun Main.

--- Rest_smarthouse/src/main/java/com/hkr/Data/ClientRun/Main.java
+++ Rest_smarthouse/src/main/java/com/hkr/Data/ClientRun/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello welcome to the football client application!");
+        System.out.println("Hello welcome to group 1 smart home client application!");
         UserInterface GUI = new UserInterface();
         GUI.menu();
     }
